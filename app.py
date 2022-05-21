09a962181efb5f800267c8918b6505e2b8f69c79
Sweety Agarwal <sweetyag142@gmail.com>
2022-05-21 19:50:26 +0530

    summarization

--- app.py
+++ app.py
@@ -169,31 +169,31 @@ def Summarize():
         def query2(payload):
             response = requests.post(API_URL_2, headers=headers, json=payload)
             return response.json()
-        def query3(payload):
-            response = requests.post(API_URL_3, headers=headers, json=payload)
-            return response.json()
-        def query4(payload):
-            response = requests.post(API_URL_4, headers=headers, json=payload)
-            return response.json()
+        # def query3(payload):
+        #     response = requests.post(API_URL_3, headers=headers, json=payload)
+        #     return response.json()
+        # def query4(payload):
+        #     response = requests.post(API_URL_4, headers=headers, json=payload)
+        #     return response.json()
 
         output = query({
             "inputs":data,
             "parameters":{"min_length":minL,"max_length":maxL},
         })[0]
-        output2 = query2({
-            "inputs":data,
-            "parameters":{"min_length":minL,"max_length":maxL},
-        })[0]
-        output3 = query3({
-            "inputs":data,
-            "parameters":{"min_length":minL,"max_length":maxL},
-        })[0]
-        output4 = query4({
-            "inputs":data,
-            "parameters":{"min_length":minL,"max_length":maxL},
-        })[0]
+        # output2 = query2({
+        #     "inputs":data,
+        #     "parameters":{"min_length":minL,"max_length":maxL},
+        # })
+        # output3 = query3({
+        #     "inputs":data,
+        #     "parameters":{"min_length":minL,"max_length":maxL},
+        # })[0]
+        # output4 = query4({
+        #     "inputs":data,
+        #     "parameters":{"min_length":minL,"max_length":maxL},
+        # })[0]
 
-        return render_template("index.html",result=output["summary_text"],result2=output2["summary_text"],result4=output4["summary_text"],result3=output3["summary_text"], )
+        return render_template("index.html",result=output["summary_text"])
     else:
         return render_template("index.html")
 
